1a41f793ed47647127f093e0357bd4724482f4a4
Harini180 <154964732+Harini180@users.noreply.github.com>
2023-12-27 21:24:19 +0530

    Update reverse_each_substring.py

--- sample/reverse_each_substring.py
+++ sample/reverse_each_substring.py
@@ -3,14 +3,14 @@ str1 = 'My Name is Jessa'
 list1 = []
 str2 = str1.split(' ',7)
 
-print(str2)
+# print(str2)
 
 for i in range(0,len(str2)):
     list1.append(str2[i][::-1])
 
-print(list1)
+# print(list1)
 
 str3 = ' '.join(list1)
 
-print(str3)
+# print(str3)
 
