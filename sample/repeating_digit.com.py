6690000ffa1e3a80cc87bc4b47fb45b5b08480fd
Harini180 <harinim34@gmail.com>
2023-12-27 17:39:30 +0530

    commit with dec_practise code

---
+++ sample/repeating_digit.com.py
@@ -0,0 +1,11 @@
+
+input_list = [1, 2, 4, 5, 5, 5, 5, 6, 5, 5, 6, 7, 7, 8, 9]
+pos = []
+search_element = 5
+for i in range(0,len(input_list)):
+    if input_list[i]==search_element and input_list[i+1]!=input_list[i] :
+        pos.append(i)
+    if input_list[i]==search_element and input_list[i-1]!=input_list[i]:
+         pos.append(i)
+         #break  (first set of repeating values index to be returned (i.e) 3 and 6 only)
+print(pos)
